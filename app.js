312bcce0531a17c3d6a24b32a51de6739381ba44
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2015-11-02 15:43:04 -0800

    Added all shops to JS and HTML

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// var ten = document.getElementById('10am');
-// var eleven = document.getElementById('11am');
-// var twelve = document.getElementById('12pm');
-// var one = document.getElementById('1pm');
-// var two = document.getElementById('2pm');
-// var three = document.getElementById('3pm');
-// var four = document.getElementById('4pm');
-// var five = document.getElementById('5pm');
-// var total = document.getElementById('total');
-
 var pikePlace = {
   times: ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
   minCustHr: 17,
@@ -41,25 +31,179 @@ var pikePlace = {
     document.getElementById('pikePlaceTotal').innerHTML = total;
   },
 
-  displayOnPage: function () {
-    // ten.innerHTML = "10am: " + this.hourlyTotals[0];
-    // eleven.innerHTML = "11am: " + this.hourlyTotals[1];
-    // twelve.innerHTML = "12pm: " + this.hourlyTotals[2];
-    // one.innerHTML = "1pm: " + this.hourlyTotals[3];
-    // two.innerHTML = "2pm: " + this.hourlyTotals[4];
-    // three.innerHTML = "3pm: " + this.hourlyTotals[5];
-    // four.innerHTML = "4pm: " + this.hourlyTotals[6];
-    // five.innerHTML = "5pm: " + this.hourlyTotals[7];
-    // total.innerHTML = "Total: " + this.dailyTotals;
+  renderAll: function () {
+    this.calcRandHrlyCust();
+    this.calcTotalPerHr();
+    this.getTotalsPerHr();
+    this.getDailyTotal();
   }
 }
 
-renderAll = function () {
-  pikePlace.calcRandHrlyCust();
-  pikePlace.calcTotalPerHr();
-  pikePlace.getTotalsPerHr();
-  pikePlace.getDailyTotal();
-  pikePlace.displayOnPage();
+var seaTac = {
+  times: ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+  minCustHr: 6,
+  maxCustHr: 44,
+  avgCookPerCust: 1.2,
+  hourlyTotals: [],
+  calcRandHrlyCust: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+
+  calcTotalPerHr: function () {
+    return this.calcRandHrlyCust() * this.avgCookPerCust;
+  },
+
+  getTotalsPerHr: function () {
+    for (var i = 0; i < 8; i++) {
+      var nextHour = document.createElement('li');
+      this.hourlyTotals[i] = this.calcTotalPerHr();
+      nextHour.innerHTML = this.times[i] + this.hourlyTotals[i];
+      document.getElementById('seaTacList').appendChild(nextHour);
+
+    }
+    return this.hourlyTotals;
+  },
+
+  getDailyTotal: function() {
+    var total = 0;
+    for (var i = 0; i < this.hourlyTotals.length; i++) {
+      total += this.hourlyTotals[i];
+    }
+    document.getElementById('seaTacTotal').innerHTML = total;
+  },
+
+  renderAll: function () {
+    this.calcRandHrlyCust();
+    this.calcTotalPerHr();
+    this.getTotalsPerHr();
+    this.getDailyTotal();
+  }
 }
 
-renderAll();
+var southCenter = {
+  times: ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+  minCustHr: 6,
+  maxCustHr: 44,
+  avgCookPerCust: 1.2,
+  hourlyTotals: [],
+  calcRandHrlyCust: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+
+  calcTotalPerHr: function () {
+    return this.calcRandHrlyCust() * this.avgCookPerCust;
+  },
+
+  getTotalsPerHr: function () {
+    for (var i = 0; i < 8; i++) {
+      var nextHour = document.createElement('li');
+      this.hourlyTotals[i] = this.calcTotalPerHr();
+      nextHour.innerHTML = this.times[i] + this.hourlyTotals[i];
+      document.getElementById('southCenterList').appendChild(nextHour);
+
+    }
+    return this.hourlyTotals;
+  },
+
+  getDailyTotal: function() {
+    var total = 0;
+    for (var i = 0; i < this.hourlyTotals.length; i++) {
+      total += this.hourlyTotals[i];
+    }
+    document.getElementById('southCenterTotal').innerHTML = total;
+  },
+
+  renderAll: function () {
+    this.calcRandHrlyCust();
+    this.calcTotalPerHr();
+    this.getTotalsPerHr();
+    this.getDailyTotal();
+  }
+}
+
+var bellevueSquare = {
+  times: ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+  minCustHr: 6,
+  maxCustHr: 44,
+  avgCookPerCust: 1.2,
+  hourlyTotals: [],
+  calcRandHrlyCust: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+
+  calcTotalPerHr: function () {
+    return this.calcRandHrlyCust() * this.avgCookPerCust;
+  },
+
+  getTotalsPerHr: function () {
+    for (var i = 0; i < 8; i++) {
+      var nextHour = document.createElement('li');
+      this.hourlyTotals[i] = this.calcTotalPerHr();
+      nextHour.innerHTML = this.times[i] + this.hourlyTotals[i];
+      document.getElementById('bellevueSquareList').appendChild(nextHour);
+
+    }
+    return this.hourlyTotals;
+  },
+
+  getDailyTotal: function() {
+    var total = 0;
+    for (var i = 0; i < this.hourlyTotals.length; i++) {
+      total += this.hourlyTotals[i];
+    }
+    document.getElementById('bellevueSquareTotal').innerHTML = total;
+  },
+
+  renderAll: function () {
+    this.calcRandHrlyCust();
+    this.calcTotalPerHr();
+    this.getTotalsPerHr();
+    this.getDailyTotal();
+  }
+}
+
+var alki = {
+  times: ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+  minCustHr: 6,
+  maxCustHr: 44,
+  avgCookPerCust: 1.2,
+  hourlyTotals: [],
+  calcRandHrlyCust: function () {
+    return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
+  },
+
+  calcTotalPerHr: function () {
+    return this.calcRandHrlyCust() * this.avgCookPerCust;
+  },
+
+  getTotalsPerHr: function () {
+    for (var i = 0; i < 8; i++) {
+      var nextHour = document.createElement('li');
+      this.hourlyTotals[i] = this.calcTotalPerHr();
+      nextHour.innerHTML = this.times[i] + this.hourlyTotals[i];
+      document.getElementById('alkiList').appendChild(nextHour);
+
+    }
+    return this.hourlyTotals;
+  },
+
+  getDailyTotal: function() {
+    var total = 0;
+    for (var i = 0; i < this.hourlyTotals.length; i++) {
+      total += this.hourlyTotals[i];
+    }
+    document.getElementById('alkiTotal').innerHTML = total;
+  },
+
+  renderAll: function () {
+    this.calcRandHrlyCust();
+    this.calcTotalPerHr();
+    this.getTotalsPerHr();
+    this.getDailyTotal();
+  }
+}
+pikePlace.renderAll();
+seaTac.renderAll();
+southCenter.renderAll();
+bellevueSquare.renderAll();
+alki.renderAll();
