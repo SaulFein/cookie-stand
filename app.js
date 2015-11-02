e589ea4519df3ec90a0380bb6b9e84f93a82d0f6
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2015-11-02 15:14:34 -0800

    JS is now creating li elements in HTML

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var ten = document.getElementById('10am');
-var eleven = document.getElementById('11am');
-var twelve = document.getElementById('12pm');
-var one = document.getElementById('1pm');
-var two = document.getElementById('2pm');
-var three = document.getElementById('3pm');
-var four = document.getElementById('4pm');
-var five = document.getElementById('5pm');
-var total = document.getElementById('total');
+// var ten = document.getElementById('10am');
+// var eleven = document.getElementById('11am');
+// var twelve = document.getElementById('12pm');
+// var one = document.getElementById('1pm');
+// var two = document.getElementById('2pm');
+// var three = document.getElementById('3pm');
+// var four = document.getElementById('4pm');
+// var five = document.getElementById('5pm');
+// var total = document.getElementById('total');
 
 var pikePlace = {
+  times: ['10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
   minCustHr: 17,
   maxCustHr: 88,
   avgCookPerCust: 5.2,
   hourlyTotals: [],
-  dailyTotals: 0,
   calcRandHrlyCust: function () {
     return Math.floor(Math.random() * (this.maxCustHr - this.minCustHr + 1) + this.minCustHr);
   },
@@ -23,29 +23,34 @@ var pikePlace = {
   },
 
   getTotalsPerHr: function () {
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 8; i++) {
+      var nextHour = document.createElement('li');
       this.hourlyTotals[i] = this.calcTotalPerHr();
+      nextHour.innerHTML = this.times[i] + this.hourlyTotals[i];
+      document.getElementById('pikePlaceList').appendChild(nextHour);
+
     }
     return this.hourlyTotals;
   },
 
   getDailyTotal: function() {
+    var total = 0;
     for (var i = 0; i < this.hourlyTotals.length; i++) {
-      this.dailyTotals += this.hourlyTotals[i];
+      total += this.hourlyTotals[i];
     }
-    return this.dailyTotals;
+    document.getElementById('pikePlaceTotal').innerHTML = total;
   },
 
   displayOnPage: function () {
-    ten.innerHTML = "10am: " + this.hourlyTotals[0];
-    eleven.innerHTML = "11am: " + this.hourlyTotals[1];
-    twelve.innerHTML = "12pm: " + this.hourlyTotals[2];
-    one.innerHTML = "1pm: " + this.hourlyTotals[3];
-    two.innerHTML = "2pm: " + this.hourlyTotals[4];
-    three.innerHTML = "3pm: " + this.hourlyTotals[5];
-    four.innerHTML = "4pm: " + this.hourlyTotals[6];
-    five.innerHTML = "5pm: " + this.hourlyTotals[7];
-    total.innerHTML = "Total: " + this.dailyTotals;
+    // ten.innerHTML = "10am: " + this.hourlyTotals[0];
+    // eleven.innerHTML = "11am: " + this.hourlyTotals[1];
+    // twelve.innerHTML = "12pm: " + this.hourlyTotals[2];
+    // one.innerHTML = "1pm: " + this.hourlyTotals[3];
+    // two.innerHTML = "2pm: " + this.hourlyTotals[4];
+    // three.innerHTML = "3pm: " + this.hourlyTotals[5];
+    // four.innerHTML = "4pm: " + this.hourlyTotals[6];
+    // five.innerHTML = "5pm: " + this.hourlyTotals[7];
+    // total.innerHTML = "Total: " + this.dailyTotals;
   }
 }
 
